2dda251805607f439fb31351e364245c1e78cef6
EmielZuurbier <emielzuurbier@outlook.com>
2016-02-05 11:37:23 +0100

    Update Emiel
    Spaties aangepast en comments toegevoegd voor suggesties en complimenten.

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -3,16 +3,19 @@
 
 	console.log('app is gestart')
 	var app = {
-		init : function () {
+		init: function () {
 			routes.init();
 			console.log("app.init()")
 		}
 	};
 
 	var routes = {
-		init : function () {
+		init: function () {
 			console.log("routes.init()");
 			window.addEventListener("hashchange", function(event) {
+				// var route = window.location.hash
+				// sections.toggle(route);
+				// (^ Dit om later makkelijker 'window.location.hash' aan te roepen.)
 				sections.toggle(window.location.hash);
 			});
 
@@ -24,6 +27,7 @@
 			console.log("sections.toggle(route)"+ route );
 			document.querySelector("section:not("+route+")").classList.remove('active');
 			document.querySelector(route).classList.add('active');
+			// ^ Leuke oplossing. Simpel en efficient.
 		}
 	};
 
