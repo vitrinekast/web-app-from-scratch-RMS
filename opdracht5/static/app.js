92d705ab48d57b2addc0e8242405379594c890c0
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-02-05 11:08:45 +0100

    Een paar schoonheids dingentjes

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -1,19 +1,19 @@
 (function () {
 	"use strict"
 
-	console.log('app is gestart')
+	console.log('app is gestart'); // Het is mooi als je consistent bent met "" of ''
 	var app = {
-		init : function () {
+		init: function () { // let op je spaties: init : --> init:
 			routes.init();
-			console.log("app.init()")
+			console.log("app.init()");
 		}
 	};
 
 	var routes = {
-		init : function () {
-			console.log("routes.init()");
-			window.addEventListener("hashchange", function(event) {
-				sections.toggle(window.location.hash);
+		init: function () { 
+			console.log("routes.init()"); // ook achter een console.log() moet een ; -> console.log(code);
+			window.addEventListener("hashchange", function(event) { // Vet dat het is gelukt om je hash als parameter mee te geven
+				sections.toggle(window.location.hash); // volgens mij is het niet erg netjes om een function in je eventListener te maken
 			});
 
 		}
@@ -21,14 +21,12 @@
 
 	var sections = {
 		toggle: function(route) {
-			console.log("sections.toggle(route)"+ route );
-			document.querySelector("section:not("+route+")").classList.remove('active');
+			console.log("sections.toggle(route)" + route); // let op de spacing tussen een +
+			document.querySelector("section:not(" + route + ")").classList.remove('active');
 			document.querySelector(route).classList.add('active');
 		}
 	};
 
 	app.init();
 
-
 })();
-
